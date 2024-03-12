0adbbfcb41a267e2876ed606ed9217bd62a2dca6
aluno <aluno@ifpe>
2024-03-12 17:23:56 -0300

    Atualização dia 12/03/2024 - ENTREGADOR

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -1,4 +1,4 @@
-package br.com.ifpe.oxefood.api.cliente;
+package br.com.ifpe.oxefood.api.entregador;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,23 +8,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.ifpe.oxefood.modelo.cliente.Cliente;
-import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
+import br.com.ifpe.oxefood.modelo.entregador.Entregador;
+import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
 
 @RestController
-@RequestMapping("/api/cliente")
+@RequestMapping("/api/entregador")
 @CrossOrigin
 
-public class ClienteController {
-
+public class EntregadorController {
+    
+    
    @Autowired
-   private ClienteService clienteService;
+   private EntregadorService entregadorService;
 
    @PostMapping
-   public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+   public ResponseEntity<Entregador> save(@RequestBody EntregadorRequest request) {
 
-       Cliente cliente = clienteService.save(request.build());
-       return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+       Entregador entregador = entregadorService.save(request.build());
+       return new ResponseEntity<Entregador>(entregador, HttpStatus.CREATED);
        
    }
 
